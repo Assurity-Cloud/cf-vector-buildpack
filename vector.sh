be26761d99cbf57c03b6a71478087524e106769f
teresachristianson <teresa.christianson@assurity.co.nz>
2023-12-14 13:24:58 +1300

    Get this back to something normal

--- vector.sh
+++ vector.sh
@@ -5,6 +5,17 @@ set -euo pipefail
 ROOT="${ROOT:-/home/vcap}"
 export APP_ROOT="${ROOT}/app"
 export VECTOR_ROOT="${VECTOR_ROOT}"
-export VECTOR_OPTS=${VECTOR_OPTS:-"--quiet"}
+export VECTOR_OPTS=${VECTOR_OPTS}
+export PROVISIONED_SERVICE_BINDING_NAMES=${PROVISIONED_SERVICE_BINDING_NAMES:-""}
 
-$VECTOR_ROOT/bin/vector $VECTOR_OPTS --config ${APP_ROOT}/*.toml
+source functions.sh
+set_provisioned_services
+
+config=""
+for f in $(ls ${APP_ROOT}/*.toml | grep -v "test-"); do
+  config="${config} -c ${f}"
+done
+
+echo "Starting vector with config files:${config}"
+
+$(${VECTOR_ROOT}/bin/vector ${VECTOR_OPTS}${config})
